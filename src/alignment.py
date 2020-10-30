71d9e8aa01ca267d79d4ad9e72593bae0cf2a75b
Rob Rouse <robrousejr@gmail.com>
2020-10-30 17:23:19 -0400

    Print out pairwise alignment

--- src/alignment.py
+++ src/alignment.py
@@ -1,5 +1,10 @@
 import fileOpener as fileReader
 from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
 
 nsp1Covid = fileReader.fileOpener("genes/covidGenome.txt", 266, 805)
 nsp1MERS = fileReader.fileOpener("genes/mersGenome.txt", 279, 857)
+
+# Print out alignment
+for a in pairwise2.align.globalxx(nsp1Covid, nsp1MERS):
+    print(format_alignment(*a))
