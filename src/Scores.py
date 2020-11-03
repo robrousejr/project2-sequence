3b31442dba8302369305ee63f896884418db9e52
Rob Rouse <robrousejr@gmail.com>
2020-11-03 14:26:36 -0500

    Create separate class files

---
+++ src/Scores.py
@@ -0,0 +1,16 @@
+# Gap, Match, Mismatch (-2, 1, -1)
+class Scores():
+    def __init__(self, match, mismatch, gap):
+        self.match = match
+        self.mismatch = mismatch
+        self.gap = gap
+    def __str__(self):
+        return 'gap, match, mismatch ({self.gap}, {self.match}, {self.mismatch})'.format(self=self)
+
+    def matchchar(self, a,b):
+        """Return the score for aligning character a with b"""
+        assert len(a) == len(b) == 1
+        if a==b:
+            return self.match
+        else:
+            return self.mismatch
--- src/alignment.py
+++ src/alignment.py
@@ -1,5 +1,5 @@
 import fileOpener as fileReader
-from functions import *
+from LocalAlignment import LocalAlignment
 
 nsp1Covid = fileReader.fileOpener("genes/covidGenome.txt", 266, 805)
 nsp1MERS = fileReader.fileOpener("genes/mersGenome.txt", 279, 857)
