a671dff42541c174a80ebe75587727e8ba030d73
Rob Rouse <robrousejr@gmail.com>
2020-11-03 13:23:06 -0500

    Add needed alignment classes
      - Remove old library usage
      - Create basic classes for scores and local alignment

--- src/alignment.py
+++ src/alignment.py
@@ -1,13 +1,9 @@
 import fileOpener as fileReader
-import functions as functions
-from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
+from functions import *
 
 nsp1Covid = fileReader.fileOpener("genes/covidGenome.txt", 266, 805)
 nsp1MERS = fileReader.fileOpener("genes/mersGenome.txt", 279, 857)
 
 print("\nPrinting out alignment\n")
 
-# Print out alignment
-for a in pairwise2.align.localxx(nsp1Covid, nsp1MERS):
-    print(format_alignment(*a))
+localAlignment = LocalAlignment(nsp1Covid, nsp1MERS, None)
